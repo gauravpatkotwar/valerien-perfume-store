398998ddf04d2f7d27805f0e5b6447b7447fe2cb
Gaurav Patkotwar <gauravpatkotwar29@gmail.com>
2026-08-09 21:19:51 +0530

    refactor: removed L'Atelier gallery page

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'index.html'),
-        atelier: resolve(__dirname, 'atelier.html')
+        main: resolve(__dirname, 'index.html')
       }
     }
   }
